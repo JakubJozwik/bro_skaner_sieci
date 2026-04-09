ea61b8800cba9e6b90d436c0521de85c17bcfad6
JakubJozwik <01197276@pw.edu.pl>
2026-04-09 18:45:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ echo "--- Rozpoczynam automatyczna instalacje systemu skanowania N01 ---"
 echo "1/5 Czyszczenie starych wersji Dockera..."
 sudo apt-get remove -y docker.io docker-compose docker-doc podman-docker containerd runc 2>/dev/null
 
-echo "2/5 Instalacja wymaganych narzedzi (Python, Curl)..."
+echo "2/5 Instalacja wymaganych narzedzi (Python, Curl, GVM-Tools)..."
 sudo apt-get update
-sudo apt-get install -y curl python3 python3-pip
+sudo apt-get install -y curl python3 python3-pip python3-gmp-parser
+sudo pip3 install gvm-tools --break-system-packages
 
 echo "3/5 Instalacja najnowszego Dockera v2 z oficjalnego zrodla..."
 curl -fsSL https://get.docker.com | sudo sh
@@ -17,10 +18,11 @@ mkdir -p ~/bso_skaner
 cd ~/bso_skaner
 rm -f docker-compose.yml compose.yaml
 curl -f -sL "https://raw.githubusercontent.com/greenbone/docs/main/src/_static/compose.yaml" -o compose.yaml
+
+# Przygotowanie czystego pliku pod skrypt w Pythonie
 touch skaner.py 
 
 echo "5/5 Uruchamianie kontenerow w tle (nowy Docker Compose v2)..."
-# Zauwaz, ze teraz komenda to "docker compose", a nie "docker-compose"
 sudo docker compose up -d
 
 echo "--- Instalacja zakonczona sukcesem! ---"
